3902a089848653cbe50dbd146e965143e085e73d
eunsueunsu <imimim93@gmail.com>
2021-02-15 17:57:25 +0900

    setting addPost

--- src/components/Posting.js
+++ src/components/Posting.js
@@ -2,27 +2,49 @@ import React from "react";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import {TextFields} from "@material-ui/icons";
+
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import {Button} from "@material-ui/core";
+import {Button, makeStyles} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+    element: {
+        margin: theme.spacing(1),
+        width: "90%"
+    },
+
+
+
+}))
 
 
 const Posting = () => {
 
+    const classes = useStyles();
     return (
-        <div>
+        <div width="100%">
             <h2>AddPost</h2>
             {/*<InputLabel htmlFor="standard-adornment-title">Title</InputLabel>*/}
             {/*<Input id="standard-adornment-title" label="Standard" placeholder="Title"></Input>*/}
-            <FormControl>
+
+            <FormControl className={classes.element}>
                 <InputLabel htmlFor="standard-adornment-password">Title</InputLabel>
-                <Input id="standard-adornment-password"/>
+                <Input id="standard-adornment-password" className={classes.element}/>
+                {/*<label> Contents</label>*/}
+                {/*<TextareaAutosize rowsMin={20} placeholder="Contents" className={classes.element}>*/}
 
-                <label> Contents</label>
-                <TextareaAutosize aria-label="minimum height" rowsMin={20} placeholder="Contents">
-
-                </TextareaAutosize>
-                <Button color="primary">SAVE
+                {/*</TextareaAutosize>*/}
+                {/* eslint-disable-next-line react/jsx-no-undef */}
+                <TextField
+                    className={classes.element}
+                    id="outlined-textarea"
+                    label="content"
+                    placeholder="Placeholder"
+                    multiline
+                    variant="outlined"
+                    rowsMin={30}
+                />
+                <Button color="primary" className={classes.element}>SAVE
                 </Button>
             </FormControl>
 
